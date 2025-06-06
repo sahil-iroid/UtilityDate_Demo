b28939158995dc478edbc515fa018a48de9ab0da
sahil-iroid <sahil@iroidsolutions.com>
2025-06-06 17:50:50 +0530

    feat: UtilityDate

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "UtilityMethods",
+    platforms: [.iOS(.v13)],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "UtilityMethods",
+            targets: ["UtilityMethods"]),
+        
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "UtilityMethods"),
+        .testTarget(
+            name: "UtilityMethodsTests",
+            dependencies: ["UtilityMethods"]
+        ),
+    ]
+)
