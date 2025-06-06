c6eaaf0da55772c03ffce54cc523fb05e79e1e6b
sahil-iroid <sahil@iroidsolutions.com>
2025-06-06 18:29:44 +0530

    fix: Swift version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.1
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -23,6 +23,5 @@ let package = Package(
             dependencies: ["UtilityMethods"]
         ),
     ],
-    swiftLanguageModes: [.v5]
 
 )
